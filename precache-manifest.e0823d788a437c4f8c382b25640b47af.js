f2185b43cf14404212cbeffccd413eca44d970f1
JC <seok0704>
2021-08-09 02:16:28 -0400

    Updates

---
+++ precache-manifest.e0823d788a437c4f8c382b25640b47af.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9e946c73e8ef8aaba0201c0397555d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c692bbdede96cfa2c75f",
+    "url": "/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "6954a7e7c9897c17402a",
+    "url": "/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "8b96b1b3d285f7d741eb",
+    "url": "/static/css/main.9c9e849b.chunk.css"
+  },
+  {
+    "revision": "d0a7b8737063bafa3c72",
+    "url": "/static/js/2.09acb2d9.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/static/js/2.09acb2d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c692bbdede96cfa2c75f",
+    "url": "/static/js/3.eee964f8.chunk.js"
+  },
+  {
+    "revision": "6954a7e7c9897c17402a",
+    "url": "/static/js/4.149606b0.chunk.js"
+  },
+  {
+    "revision": "8b96b1b3d285f7d741eb",
+    "url": "/static/js/main.d7326404.chunk.js"
+  },
+  {
+    "revision": "77fa3f5507e2fe4720f7",
+    "url": "/static/js/runtime-main.58b0d4bf.js"
+  },
+  {
+    "revision": "9e30732750bd06bd648a3f0300568516",
+    "url": "/static/media/1591628761190.9e307327.jfif"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4fea85464f6e8e12afe79fcdb90e0fe2",
+    "url": "/static/media/amphenol.4fea8546.jpg"
+  },
+  {
+    "revision": "f8e820dc6985763e0d2cba9f82640ff4",
+    "url": "/static/media/banana.f8e820dc.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "5fe72036a7ad426dd3c70f3e11d8f810",
+    "url": "/static/media/gpu.5fe72036.jpg"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "1c66220c6149b49352c4cf496f70ad86",
+    "url": "/static/media/leagueoflegends.1c66220c.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "41736f27da5ce3b65fe481f8f0410a3a",
+    "url": "/static/media/nvidia.41736f27.png"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "506dc1db13a3cd87e2a4509b13822109",
+    "url": "/static/media/teslaLogo.506dc1db.png"
+  }
+]);
